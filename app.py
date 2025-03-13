27f8c5b4b0f050c8b00011525b1f0ff12c3453bc
alerie555 <alexej.riemer@ergo.com>
2025-03-13 16:18:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ flask_app = Flask(__name__)
 def hello_world():
     chart_data = pd.DataFrame({'Apps': [x for x in range(30)],
                                'Fun with data': [2 ** x for x in range(30)]})
-    return f'<h1>Hello, World!</h1> {chart_data.to_html(index=False)}'
+    return f'<h1>Hallo Welt!!!!</h1> {chart_data.to_html(index=False)}'
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
